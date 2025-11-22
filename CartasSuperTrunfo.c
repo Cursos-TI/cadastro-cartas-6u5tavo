20a88f230a77e9e5d1941cbc26d6b667b86e25ce
gustavo <smgustavo09@gmail.com>
2025-11-22 18:21:08 +0000

    atualiazado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,12 @@ int main() {
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-      int populacao, pontost;
+      int pontost, populacao;
       char estado[8];
       char codigo[4];
       char cidade[50];
       float area, pib;
+      float densidade, pibcapita;
 
   // Área para entrada de dados
 
@@ -37,6 +38,11 @@ int main() {
       printf ("Digite o número de pontos turísticos: \n");
       scanf ("%d", &pontost);
 
+  // Área para calculo da densidade populacional e PIB per capita
+
+      densidade = (populacao/area);
+      pibcapita = (pib/populacao);
+
   // Área para exibição dos dados da cidade
 
       printf ("Estado: %s \n", estado);
@@ -46,6 +52,43 @@ int main() {
       printf ("Área: %.2f km² \n", area);
       printf ("PIB: %.2f bilhões de reais \n", pib);
       printf ("Número de Pontos Turísticos: %d \n", pontost);
+      printf ("Densidade Populacional: %.2f hab/km² \n", densidade);
+      printf ("PIB per Capita: %.11f reais \n", pibcapita);
+
+
+      printf ("Digite o estado: \n");
+      scanf ("%s", &estado);
+
+      printf ("Digite o codigo da carta: \n");
+      scanf ("%s", &codigo);
+
+      printf ("Digite o nome da cidade: \n");
+      scanf ("%s", &cidade);
+
+      printf ("Digite a população: \n");
+      scanf ("%d", &populacao);
+
+      printf ("Digite a área (em km²): \n");
+      scanf ("%f", &area);
+
+      printf ("Digite o PIB: \n");
+      scanf ("%f", &pib);
+
+      printf ("Digite o número de pontos turísticos: \n");
+      scanf ("%d", &pontost);
+
+      densidade = (populacao/area);
+      pibcapita = (pib/populacao);
+
+      printf ("Estado: %s \n", estado);
+      printf ("Código da Carta: %s \n", codigo);
+      printf ("Nome da Cidade: %s \n", cidade);
+      printf ("População: %d \n", populacao);
+      printf ("Área: %.2f km² \n", area);
+      printf ("PIB: %.2f bilhões de reais \n", pib);
+      printf ("Número de Pontos Turísticos: %d \n", pontost);
+      printf ("Densidade Populacional: %.2f hab/km² \n", densidade);
+      printf ("PIB per Capita: %.11f reais \n", pibcapita);
 
 return 0;
 } 
